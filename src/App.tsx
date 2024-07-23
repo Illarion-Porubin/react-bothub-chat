561abe911ec781a1aaac6b12f40a06068bdf6214
lars <geniuslar@mail.ru>
2024-07-23 14:07:43 +0300

    updated the chat and added various fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Home from "./pages/home/Home";
 import "./App.scss";
-// import Test from "./pages/test/Test";
 
 function App() {
   return (
     <>
       <Home />
-      {/* <Test /> */}
     </>
   );
 }
